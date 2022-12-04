2a7bf92df32e56dc6ce1a59afcc1752f336cc9c3
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-12-04 20:38:10 +0700

    first commit

---
+++ src/main/java/simplerestfull/projectrestfull/ProjectrestfullApplication.java
@@ -0,0 +1,13 @@
+package simplerestfull.projectrestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectrestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectrestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simplerestfull/projectrestfull/ProjectrestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package simplerestfull.projectrestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectrestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
