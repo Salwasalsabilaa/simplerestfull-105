6f66360fe6a8d1c235be02aa5525d2ff4eeb657e
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-12-04 20:49:47 +0700

    third commit

--- src/main/java/simplerestfull/projectrestfull/product.java
+++ src/main/java/simplerestfull/projectrestfull/product.java
@@ -10,5 +10,23 @@ package simplerestfull.projectrestfull;
  * @author Inspiron
  */
 public class product {
+    private String id;
+    private String name;
     
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
